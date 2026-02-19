47755daaa8e811378f3ca880b2b6b6bbc4543e2f
David Dangyil <daviddangyil@gmail.com>
2026-02-19 12:38:24 +0100

    clear unwanted files

--- frontend/src/api/config.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API = axios.create({
-  baseURL: 'https://react-form-validation-api-4ist.onrender.com/api',
-  headers: { 'Content-Type': 'application/json' }
-});
-
-export default API;
